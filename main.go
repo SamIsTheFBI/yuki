3ddc650ff8759b16b1a9f537533df37cec6d6a07
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-08-10 19:56:23 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BindFile struct {
+	File *multipart.FileHeader `form:"file" binding:"required"`
+}
+
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"name": "yuki",
+		})
+	})
+
+	router.POST("/", func(c *gin.Context) {
+		var bindFile BindFile
+
+		// Bind file
+		if err := c.ShouldBind(&bindFile); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("err: %s", err.Error()))
+			return
+		}
+
+		// Save uploaded file
+		file := bindFile.File
+		dst := filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, dst); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully.", file.Filename))
+	})
+	router.Run(":8080")
+}
