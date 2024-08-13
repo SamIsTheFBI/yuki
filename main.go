958be526221d3f7bf0e427e32083886f660b6027
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-08-13 20:34:29 +0530

    feat: add basic post & get file

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 type BindFile struct {
@@ -21,9 +22,14 @@ func main() {
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"name": "yuki",
-		})
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.GET("/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		file := filepath.Join("uploads", id)
+		c.File(file)
 	})
 
 	router.POST("/", func(c *gin.Context) {
@@ -36,14 +42,21 @@ func main() {
 		}
 
 		// Save uploaded file
+		err := os.Mkdir("uploads", 0755)
+		if os.IsNotExist(err) {
+			os.Mkdir("uploads", 0755)
+		}
+
 		file := bindFile.File
-		dst := filepath.Base(file.Filename)
+		dst := filepath.Join("uploads", filepath.Base(file.Filename))
 		if err := c.SaveUploadedFile(file, dst); err != nil {
 			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
 			return
 		}
 
-		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully.", file.Filename))
+		hostname := c.Request.Host
+		url := hostname + `/` + file.Filename
+		c.String(http.StatusOK, fmt.Sprintf("%s", url))
 	})
 	router.Run(":8080")
 }
